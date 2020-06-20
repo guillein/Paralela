43bfe294bae5b851dc3420d62f8bee2f950ab28d
guillein <66335370+guillein@users.noreply.github.com>
2020-06-20 12:03:41 +0200

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -2268,7 +2268,7 @@ class DataFrame(PandasMapOpsMixin, PandasConversionMixin):
         if isinstance(color, str):
             # Un solo color para el plot
             ax.scatter([p.x for p in puntos], [p.y for p in puntos], c = color, **kwargs)
-        if not isinstance(color, str) and color != False:
+        elif not isinstance(color, str) and color != False:
             ax.scatter([p.x for p, _ in puntos], [p.y for p, _ in puntos], c = [c for _, c in puntos], **kwargs)
         else:
             ax.scatter([p.x for p in puntos], [p.y for p in puntos], **kwargs)
